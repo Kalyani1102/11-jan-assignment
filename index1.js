58295e3d67dc510029e0448f76ed62b9ee5c6076
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2022-01-16 15:12:08 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,12 @@
+// Create a pattern on the page. take an input using prompt.
+
+var n = parseInt(prompt("Enter a Number"));
+
+for ( var i=1 ; i<=n ; i++){
+
+    for(var j=1;j<i+1;j++){
+       document.write("* ");
+    }
+    document.write("<br>");
+    
+}
